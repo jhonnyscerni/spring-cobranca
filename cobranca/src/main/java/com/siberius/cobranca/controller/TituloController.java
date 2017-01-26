f4e6a83323e1958dcee98314fe2a9390804be5e8
jhonnyscerni@gmail.com <jhonnyscerni@gmail.com>
2017-01-26 11:24:03 -0300

    Ajustes nos valores do formulario

--- cobranca/src/main/java/com/siberius/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/siberius/cobranca/controller/TituloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.siberius.cobranca.model.StatusTitulo;
 import com.siberius.cobranca.model.Titulo;
@@ -39,16 +40,15 @@ public class TituloController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors erros) {
+	public String salvar(@Validated Titulo titulo, Errors erros, RedirectAttributes attributes) {
 
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		if (erros.hasErrors()) {
-			return mv;
+			return "CadastroTitulo";
 		}
 		
 		titulos.save(titulo);
-		mv.addObject("mensagem", "Título salvo com sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
 
 	@ModelAttribute("statusTitulo")
